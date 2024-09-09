0eb15e694e05a39cda640c09c94a4e07bf1cc446
AMN <116947899+losercodes@users.noreply.github.com>
2024-09-10 03:50:43 +0530

    Update FormSection.vue

--- components/Base/FormSection.vue
+++ components/Base/FormSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md:grid md:grid-cols-3 md:gap-6">
     <div class="md:col-span-1">
-      <div class="px-4 sm:px-0 sticky top-0">
+      <div class="px-4 sm:px-0 sticky top-0 bg-white shadow-md rounded-lg p-6">
         <h3 class="text-lg font-medium leading-6 text-gray-900">
           {{ title }}
         </h3>
@@ -12,7 +12,9 @@
       </div>
     </div>
     <div class="mt-5 md:col-span-2 md:mt-0 relative">
-      <slot />
+      <div class="bg-white shadow-md rounded-lg p-6">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
