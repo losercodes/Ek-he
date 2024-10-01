a6af2f96ac4af9308d5b8863f3eb201f2fe0b7eb
KUMAR KUSH <115366430+KUMAR-KUSH@users.noreply.github.com>
2024-10-01 19:45:40 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="h-screen grid grid-cols-3 divide-x">
+    <!-- Left section for the form input and preview buttons -->
     <div class="col-span-2 h-screen flex flex-col bg-gradient-to-b from-slate-100 to-slate-200">
       <div class="flex-1 overflow-y-auto p-8">
+        <!-- Profile form component -->
         <app-form-profile
           v-model:name="data.n"
           v-model:desc="data.d"
           v-model:image="data.i"
         />
         <app-form-hr />
+        <!-- Social links form component -->
         <app-form-social-links
           v-model:facebook="data.f"
           v-model:twitter="data.t"
@@ -20,9 +23,13 @@
           v-model:youtube="data.y"
         />
         <app-form-hr />
+        <!-- Additional links form component -->
         <app-form-links v-model="data.ls" />
       </div>
+      
+      <!-- Button section for actions like adding demo data, publishing -->
       <div class="border-t bg-white flex items-center shadow-md rounded-lg p-2">
+        <!-- Add demo data button -->
         <button
           @click="prefillDemoData"
           class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium text-white bg-blue-500 hover:bg-blue-600 transition duration-200 rounded-lg"
@@ -30,6 +37,7 @@
           <span> Add demo data </span>
           <icon name="mdi:code-json" class="h-4 w-4" />
         </button>
+        <!-- Publish button -->
         <button
           @click="publish"
           class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium text-white bg-green-500 hover:bg-green-600 transition duration-200 rounded-lg"
@@ -37,6 +45,7 @@
           <span> Publish </span>
           <icon name="ph:paper-plane-tilt-bold" class="h-4 w-4" />
         </button>
+        <!-- Github link -->
         <a
           href="https://github.com/losercodes/Ek-he"
           target="_blank"
@@ -47,7 +56,11 @@
         </a>
       </div>
     </div>
+    
+    <!-- Preview section displaying the filled data -->
     <app-form-preview :data="data" />
+    
+    <!-- Attribution link at the bottom right -->
     <a
       href="https://twitter.com/AmanPandey_x"
       target="_blank"
@@ -62,37 +75,39 @@
 import { ref } from 'vue';
 import { encodeData } from "../utils/transformer";
 
+// Initializing data with ref to make it reactive
 const data = ref({
-  n: "",
-  d: "",
-  i: "",
-  f: "",
-  t: "",
-  ig: "",
-  gh: "",
-  tg: "",
-  l: "",
-  e: "",
-  w: "",
-  y: "",
-  ls: [],
+  n: "",  // Name field
+  d: "",  // Description field
+  i: "",  // Image URL
+  f: "",  // Facebook link
+  t: "",  // Twitter link
+  ig: "", // Instagram link
+  gh: "", // Github link
+  tg: "", // Telegram link
+  l: "",  // LinkedIn link
+  e: "",  // Email
+  w: "",  // WhatsApp number
+  y: "",  // YouTube link
+  ls: [], // Additional links list
 });
 
+// Function to prefill the form with demo data
 const prefillDemoData = () => {
   data.value = {
-    n: "John",
-    d: "I’m John, the king in the north. I know Nothing.",
-    i: "https://i.insider.com/56743fad72f2c12a008b6cc0",
-    f: "https://www.facebook.com/john_snow",
-    t: "https://twitter.com/john_snow",
-    ig: "https://www.instagram.com/john_snow",
-    e: "mail@john_snow.cc",
-    gh: "https://github.com/john_snow",
-    tg: "https://t.me/john_snow",
-    w: "+918888888888",
-    y: "https://youtube.com/@john_snow",
-    l: "https://linkedin.com/john_snow",
-    ls: [
+    n: "John", // Prefilled name
+    d: "I’m John, the king in the north. I know Nothing.", // Prefilled description
+    i: "https://i.insider.com/56743fad72f2c12a008b6cc0", // Prefilled image URL
+    f: "https://www.facebook.com/john_snow", // Prefilled Facebook link
+    t: "https://twitter.com/john_snow", // Prefilled Twitter link
+    ig: "https://www.instagram.com/john_snow", // Prefilled Instagram link
+    e: "mail@john_snow.cc", // Prefilled email
+    gh: "https://github.com/john_snow", // Prefilled Github link
+    tg: "https://t.me/john_snow", // Prefilled Telegram link
+    w: "+918888888888", // Prefilled WhatsApp number
+    y: "https://youtube.com/@john_snow", // Prefilled YouTube link
+    l: "https://linkedin.com/john_snow", // Prefilled LinkedIn link
+    ls: [  // Prefilled additional links
       {
         l: "My Website",
         i: "ph:globe-duotone",
@@ -122,6 +137,7 @@ const prefillDemoData = () => {
   };
 };
 
+// Function to publish data and copy the link to the clipboard
 const publish = () => {
   const url = `${window.location.origin}/1?data=${encodeData(data.value)}`;
   navigator.clipboard.writeText(url).then(() => {
