19a34e2c3ccb0967d8559665283ceed01f6f0501
KUMAR KUSH <115366430+KUMAR-KUSH@users.noreply.github.com>
2024-10-01 20:00:03 +0530

    Update 1.vue

--- pages/1.vue
+++ pages/1.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
+    <!-- If decodedData is available, render the templates-simple component -->
     <templates-simple v-if="decodedData" :acc="decodedData" />
+    
+    <!-- If decodedData is not available, show a loading spinner in the center of the screen -->
     <div
       v-else
       class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
@@ -9,11 +12,13 @@
     </div>
   </div>
 </template>
+
 <script setup>
-import { decodeData } from "../utils/transformer";
-const route = useRoute();
-const acc = route.query.data;
-const decodedData = ref({});
+import { decodeData } from "../utils/transformer"; // Importing the data decoding utility
+const route = useRoute(); // Getting the current route information
+const acc = route.query.data; // Extracting 'data' from the query parameters in the route
+const decodedData = ref({}); // Declaring a reactive reference for storing decoded data
+
+// Decoding the 'data' parameter and assigning it to decodedData
 decodedData.value = decodeData(acc);
 </script>
-<style scoped></style>
